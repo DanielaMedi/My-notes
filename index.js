369894aeb3946baceeef981502a630d3436880d8
Daniela Medina <daniela.mb0709@gmail.com>
2024-04-23 07:46:15 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const notesList = [
+    {
+        date: "23/04/2024",
+        title: "My first note",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, consequatur, veniam earum praesentium deleniti esse."
+    },
+
+    {
+        date: "20/04/2024",
+        title: "My favorite note",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, consequatur, veniam earum praesentium deleniti esse."
+    }
+]
+
+const notesElemente = document.querySelector ("my__note");
